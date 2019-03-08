e7a9155fcbd8ef108a9446d55c82e97219965887
AndriusCim <cimermonas.andrius@gmail.com>
2019-03-08 09:15:51 +0200

    Added fixed navbar with burger

--- assets/js/video.js
+++ assets/js/video.js
@@ -7,3 +7,11 @@ $(document).ready(function() {
       }
     });
   });
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "navbar") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar";
+    }
+  }
